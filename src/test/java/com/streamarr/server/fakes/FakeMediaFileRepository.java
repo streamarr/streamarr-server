03741868eca2eb1c761a9b12fe43a78536465bd8
Andrew Stucky <andrew.david.stucky@gmail.com>
2026-02-04 16:19:43 -0500

    structural: add findByMediaId to MediaFileRepository

--- src/test/java/com/streamarr/server/fakes/FakeMediaFileRepository.java
+++ src/test/java/com/streamarr/server/fakes/FakeMediaFileRepository.java
@@ -2,7 +2,9 @@ package com.streamarr.server.fakes;
 
 import com.streamarr.server.domain.media.MediaFile;
 import com.streamarr.server.repositories.media.MediaFileRepository;
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public class FakeMediaFileRepository extends FakeJpaRepository<MediaFile>
     implements MediaFileRepository {
@@ -11,5 +13,12 @@ public class FakeMediaFileRepository extends FakeJpaRepository<MediaFile>
     return database.values().stream()
         .filter(file -> filepath.equals(file.getFilepath()))
         .findFirst();
+  }
+
+  @Override
+  public List<MediaFile> findByMediaId(UUID mediaId) {
+    return database.values().stream()
+        .filter(file -> mediaId.equals(file.getMediaId()))
+        .toList();
   }
 }
