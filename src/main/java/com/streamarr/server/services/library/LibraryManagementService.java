8745a106db3c55a366fc24f9941883a271216519
Andrew Stucky <andrew.david.stucky@gmail.com>
2026-02-05 12:27:24 -0500

    behavioral: simplify addLibrary to use toBuilder()
    Replace manual builder field copying with toBuilder() for cleaner code.

--- src/main/java/com/streamarr/server/services/library/LibraryManagementService.java
+++ src/main/java/com/streamarr/server/services/library/LibraryManagementService.java
@@ -87,15 +87,7 @@ public class LibraryManagementService {
     validateFilepathNotAlreadyUsed(library.getFilepath());
     validatePathExistsAndIsDirectory(library.getFilepath());
 
-    var libraryToSave =
-        Library.builder()
-            .name(library.getName())
-            .filepath(library.getFilepath())
-            .backend(library.getBackend())
-            .type(library.getType())
-            .externalAgentStrategy(library.getExternalAgentStrategy())
-            .status(LibraryStatus.HEALTHY)
-            .build();
+    var libraryToSave = library.toBuilder().status(LibraryStatus.HEALTHY).build();
     var savedLibrary = libraryRepository.save(libraryToSave);
 
     triggerAsyncScan(savedLibrary.getId());
