724b071658f3ff24cc49922b8cdd548d426dbef0
Andrew Stucky <andrew.david.stucky@gmail.com>
2026-02-06 15:19:27 -0500

    behavioral: add Relay cursor pagination for Series

--- src/main/java/com/streamarr/server/repositories/media/SeriesRepositoryCustom.java
+++ src/main/java/com/streamarr/server/repositories/media/SeriesRepositoryCustom.java
@@ -1,9 +1,15 @@
 package com.streamarr.server.repositories.media;
 
 import com.streamarr.server.domain.media.Series;
+import com.streamarr.server.graphql.cursor.MediaPaginationOptions;
+import java.util.List;
 import java.util.Optional;
 
 public interface SeriesRepositoryCustom {
 
   Optional<Series> findByTmdbId(String tmdbId);
+
+  List<Series> seekWithFilter(MediaPaginationOptions options);
+
+  List<Series> findFirstWithFilter(MediaPaginationOptions options);
 }
