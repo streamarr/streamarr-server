1091763614edfa11c4207669a3168e2d61a9118f
Andrew Stucky <andrew.david.stucky@gmail.com>
2026-02-08 17:20:50 -0500

    structural: extract duplicated createTestImage to TestImages utility
    Move the identical createTestImage helper out of ImageServiceTest,
    ImageEnrichmentListenerTest, and ImageVariantServiceTest into a
    shared TestImages utility class in the fakes package.

--- src/test/java/com/streamarr/server/services/metadata/ImageEnrichmentListenerTest.java
+++ src/test/java/com/streamarr/server/services/metadata/ImageEnrichmentListenerTest.java
@@ -1,5 +1,6 @@
 package com.streamarr.server.services.metadata;
 
+import static com.streamarr.server.fakes.TestImages.createTestImage;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.google.common.jimfs.Configuration;
@@ -14,13 +15,9 @@ import com.streamarr.server.services.ImageService;
 import com.streamarr.server.services.concurrency.MutexFactoryProvider;
 import com.streamarr.server.services.metadata.events.ImageSource.TmdbImageSource;
 import com.streamarr.server.services.metadata.events.MetadataEnrichedEvent;
-import java.awt.Color;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
-import javax.imageio.ImageIO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -162,21 +159,6 @@ class ImageEnrichmentListenerTest {
 
     var images = imageRepository.findByEntityIdAndEntityType(entityId, ImageEntityType.MOVIE);
     assertThat(images).isEmpty();
-  }
-
-  private byte[] createTestImage(int width, int height) {
-    var image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-    var graphics = image.createGraphics();
-    graphics.setColor(Color.BLUE);
-    graphics.fillRect(0, 0, width, height);
-    graphics.dispose();
-
-    try (var outputStream = new ByteArrayOutputStream()) {
-      ImageIO.write(image, "jpg", outputStream);
-      return outputStream.toByteArray();
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
   }
 
   private static class FakeTmdbHttpService extends TheMovieDatabaseHttpService {
--- src/test/java/com/streamarr/server/services/metadata/ImageVariantServiceTest.java
+++ src/test/java/com/streamarr/server/services/metadata/ImageVariantServiceTest.java
@@ -1,5 +1,6 @@
 package com.streamarr.server.services.metadata;
 
+import static com.streamarr.server.fakes.TestImages.createTestImage;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
@@ -11,9 +12,7 @@ import static org.mockito.Mockito.when;
 import com.streamarr.server.domain.media.ImageSize;
 import com.streamarr.server.domain.media.ImageType;
 import com.streamarr.server.exceptions.ImageProcessingException;
-import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import javax.imageio.ImageIO;
@@ -254,18 +253,4 @@ class ImageVariantServiceTest {
     }
   }
 
-  private byte[] createTestImage(int width, int height) {
-    var image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-    var graphics = image.createGraphics();
-    graphics.setColor(Color.BLUE);
-    graphics.fillRect(0, 0, width, height);
-    graphics.dispose();
-
-    try (var outputStream = new ByteArrayOutputStream()) {
-      ImageIO.write(image, "jpg", outputStream);
-      return outputStream.toByteArray();
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
 }
