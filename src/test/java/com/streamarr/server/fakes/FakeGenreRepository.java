4d5aeb3eaccd484c29ba52c4992a51d79e85e873
Andrew Stucky <andrew.david.stucky@gmail.com>
2026-02-09 09:33:41 -0500

    structural: add FakeGenreRepository for unit testing

---
+++ src/test/java/com/streamarr/server/fakes/FakeGenreRepository.java
@@ -0,0 +1,28 @@
+package com.streamarr.server.fakes;
+
+import com.streamarr.server.domain.metadata.Genre;
+import com.streamarr.server.repositories.GenreRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.apache.commons.lang3.NotImplementedException;
+
+public class FakeGenreRepository extends FakeJpaRepository<Genre> implements GenreRepository {
+
+  @Override
+  public Optional<Genre> findBySourceId(String sourceId) {
+    return database.values().stream()
+        .filter(genre -> sourceId.equals(genre.getSourceId()))
+        .findFirst();
+  }
+
+  @Override
+  public List<Genre> findByMovieId(UUID movieId) {
+    throw new NotImplementedException();
+  }
+
+  @Override
+  public List<Genre> findBySeriesId(UUID seriesId) {
+    throw new NotImplementedException();
+  }
+}
