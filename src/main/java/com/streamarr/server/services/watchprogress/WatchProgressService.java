1740e1aae6ce3d9ad693106eba7f9ebb492fc7fb
Andrew Stucky <andrew.david.stucky@gmail.com>
2026-04-02 09:51:06 -0400

    structural: migrate WatchProgressService to @Service and delete WatchProgressConfig

--- src/main/java/com/streamarr/server/services/watchprogress/WatchProgressService.java
+++ src/main/java/com/streamarr/server/services/watchprogress/WatchProgressService.java
@@ -27,8 +27,10 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
 
 @Slf4j
+@Service
 @RequiredArgsConstructor
 public class WatchProgressService {
 
