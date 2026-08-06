7c4debc4b6cdd22e63bd46b77c20493984ce60e8
Andrew Stucky <andrew.david.stucky@gmail.com>
2026-08-06 14:12:30 -0400

    structural: move company relationship reads to jOOQ

--- src/main/java/com/streamarr/server/repositories/CompanyRepositoryCustomImpl.java
+++ src/main/java/com/streamarr/server/repositories/CompanyRepositoryCustomImpl.java
@@ -1,7 +1,13 @@
 package com.streamarr.server.repositories;
 
 import static com.streamarr.server.jooq.generated.tables.Company.COMPANY;
+import static com.streamarr.server.jooq.generated.tables.MovieCompany.MOVIE_COMPANY;
+import static com.streamarr.server.jooq.generated.tables.SeriesCompany.SERIES_COMPANY;
 
+import com.streamarr.server.domain.metadata.Company;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
@@ -12,6 +18,7 @@ public class CompanyRepositoryCustomImpl implements CompanyRepositoryCustom {
 
   private final DSLContext dsl;
   private final AuditorAware<UUID> auditorAware;
+  @PersistenceContext private final EntityManager entityManager;
 
   @Override
   public boolean insertIfAbsent(String sourceId, String name) {
@@ -26,5 +33,29 @@ public class CompanyRepositoryCustomImpl implements CompanyRepositoryCustom {
             .doNothing()
             .execute();
     return rowsAffected > 0;
+  }
+
+  @Override
+  public List<Company> findByMovieId(UUID movieId) {
+    var query =
+        dsl.select(COMPANY.asterisk())
+            .from(COMPANY)
+            .innerJoin(MOVIE_COMPANY)
+            .on(MOVIE_COMPANY.COMPANY_ID.eq(COMPANY.ID))
+            .where(MOVIE_COMPANY.MOVIE_ID.eq(movieId));
+
+    return JooqQueryHelper.nativeQuery(entityManager, query, Company.class);
+  }
+
+  @Override
+  public List<Company> findBySeriesId(UUID seriesId) {
+    var query =
+        dsl.select(COMPANY.asterisk())
+            .from(COMPANY)
+            .innerJoin(SERIES_COMPANY)
+            .on(SERIES_COMPANY.COMPANY_ID.eq(COMPANY.ID))
+            .where(SERIES_COMPANY.SERIES_ID.eq(seriesId));
+
+    return JooqQueryHelper.nativeQuery(entityManager, query, Company.class);
   }
 }
---
+++ src/test/java/com/streamarr/server/repositories/SeriesRelationshipRepositoryIT.java
@@ -0,0 +1,43 @@
+package com.streamarr.server.repositories;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.streamarr.server.AbstractIntegrationTest;
+import com.streamarr.server.domain.media.Series;
+import com.streamarr.server.domain.metadata.Company;
+import com.streamarr.server.fixtures.LibraryFixtureCreator;
+import com.streamarr.server.repositories.media.SeriesRepository;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Tag("IntegrationTest")
+@DisplayName("Series Relationship Repository Integration Tests")
+class SeriesRelationshipRepositoryIT extends AbstractIntegrationTest {
+
+  @Autowired private SeriesRepository seriesRepository;
+  @Autowired private LibraryRepository libraryRepository;
+  @Autowired private CompanyRepository companyRepository;
+
+  @Test
+  @DisplayName("Should find studios by series ID")
+  void shouldFindStudiosBySeriesId() {
+    var library = libraryRepository.save(LibraryFixtureCreator.buildFakeLibrary());
+    var studio =
+        companyRepository.save(
+            Company.builder().name("HBO Entertainment").sourceId("hbo-1").build());
+    var series =
+        seriesRepository.saveAndFlush(
+            Series.builder()
+                .title("The Sopranos")
+                .library(library)
+                .studios(Set.of(studio))
+                .build());
+
+    var studios = companyRepository.findBySeriesId(series.getId());
+
+    assertThat(studios).singleElement().extracting(Company::getName).isEqualTo("HBO Entertainment");
+  }
+}
