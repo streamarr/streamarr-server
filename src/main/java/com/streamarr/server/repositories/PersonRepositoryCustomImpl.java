e5c1d1ebc0197f2928f6edc838230fdfce686d6b
Andrew Stucky <andrew.david.stucky@gmail.com>
2026-08-06 14:23:44 -0400

    structural: move person relationship reads to jOOQ

--- src/main/java/com/streamarr/server/repositories/PersonRepositoryCustomImpl.java
+++ src/main/java/com/streamarr/server/repositories/PersonRepositoryCustomImpl.java
@@ -1,7 +1,15 @@
 package com.streamarr.server.repositories;
 
+import static com.streamarr.server.jooq.generated.tables.MovieDirector.MOVIE_DIRECTOR;
+import static com.streamarr.server.jooq.generated.tables.MoviePerson.MOVIE_PERSON;
 import static com.streamarr.server.jooq.generated.tables.Person.PERSON;
+import static com.streamarr.server.jooq.generated.tables.SeriesDirector.SERIES_DIRECTOR;
+import static com.streamarr.server.jooq.generated.tables.SeriesPerson.SERIES_PERSON;
 
+import com.streamarr.server.domain.metadata.Person;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
@@ -12,6 +20,7 @@ public class PersonRepositoryCustomImpl implements PersonRepositoryCustom {
 
   private final DSLContext dsl;
   private final AuditorAware<UUID> auditorAware;
+  @PersistenceContext private final EntityManager entityManager;
 
   @Override
   public boolean insertIfAbsent(String sourceId, String name) {
@@ -26,5 +35,53 @@ public class PersonRepositoryCustomImpl implements PersonRepositoryCustom {
             .doNothing()
             .execute();
     return rowsAffected > 0;
+  }
+
+  @Override
+  public List<Person> findCastByMovieId(UUID movieId) {
+    var query =
+        dsl.select(PERSON.asterisk())
+            .from(PERSON)
+            .innerJoin(MOVIE_PERSON)
+            .on(MOVIE_PERSON.PERSON_ID.eq(PERSON.ID))
+            .where(MOVIE_PERSON.MOVIE_ID.eq(movieId));
+
+    return JooqQueryHelper.nativeQuery(entityManager, query, Person.class);
+  }
+
+  @Override
+  public List<Person> findDirectorsByMovieId(UUID movieId) {
+    var query =
+        dsl.select(PERSON.asterisk())
+            .from(PERSON)
+            .innerJoin(MOVIE_DIRECTOR)
+            .on(MOVIE_DIRECTOR.PERSON_ID.eq(PERSON.ID))
+            .where(MOVIE_DIRECTOR.MOVIE_ID.eq(movieId));
+
+    return JooqQueryHelper.nativeQuery(entityManager, query, Person.class);
+  }
+
+  @Override
+  public List<Person> findCastBySeriesId(UUID seriesId) {
+    var query =
+        dsl.select(PERSON.asterisk())
+            .from(PERSON)
+            .innerJoin(SERIES_PERSON)
+            .on(SERIES_PERSON.PERSON_ID.eq(PERSON.ID))
+            .where(SERIES_PERSON.SERIES_ID.eq(seriesId));
+
+    return JooqQueryHelper.nativeQuery(entityManager, query, Person.class);
+  }
+
+  @Override
+  public List<Person> findDirectorsBySeriesId(UUID seriesId) {
+    var query =
+        dsl.select(PERSON.asterisk())
+            .from(PERSON)
+            .innerJoin(SERIES_DIRECTOR)
+            .on(SERIES_DIRECTOR.PERSON_ID.eq(PERSON.ID))
+            .where(SERIES_DIRECTOR.SERIES_ID.eq(seriesId));
+
+    return JooqQueryHelper.nativeQuery(entityManager, query, Person.class);
   }
 }
--- src/test/java/com/streamarr/server/repositories/SeriesRelationshipRepositoryIT.java
+++ src/test/java/com/streamarr/server/repositories/SeriesRelationshipRepositoryIT.java
@@ -6,8 +6,10 @@ import com.streamarr.server.AbstractIntegrationTest;
 import com.streamarr.server.domain.media.Series;
 import com.streamarr.server.domain.metadata.Company;
 import com.streamarr.server.domain.metadata.Genre;
+import com.streamarr.server.domain.metadata.Person;
 import com.streamarr.server.fixtures.LibraryFixtureCreator;
 import com.streamarr.server.repositories.media.SeriesRepository;
+import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -22,6 +24,7 @@ class SeriesRelationshipRepositoryIT extends AbstractIntegrationTest {
   @Autowired private LibraryRepository libraryRepository;
   @Autowired private CompanyRepository companyRepository;
   @Autowired private GenreRepository genreRepository;
+  @Autowired private PersonRepository personRepository;
 
   @Test
   @DisplayName("Should find studios by series ID")
@@ -56,5 +59,41 @@ class SeriesRelationshipRepositoryIT extends AbstractIntegrationTest {
     var genres = genreRepository.findBySeriesId(series.getId());
 
     assertThat(genres).singleElement().extracting(Genre::getName).isEqualTo("Crime");
+  }
+
+  @Test
+  @DisplayName("Should find cast by series ID")
+  void shouldFindCastBySeriesId() {
+    var library = libraryRepository.save(LibraryFixtureCreator.buildFakeLibrary());
+    var actor =
+        personRepository.save(
+            Person.builder().name("Series Actor").sourceId("series-actor-1").build());
+    var series =
+        seriesRepository.saveAndFlush(
+            Series.builder().title("Cast Series").library(library).cast(List.of(actor)).build());
+
+    var cast = personRepository.findCastBySeriesId(series.getId());
+
+    assertThat(cast).singleElement().extracting(Person::getName).isEqualTo("Series Actor");
+  }
+
+  @Test
+  @DisplayName("Should find directors by series ID")
+  void shouldFindDirectorsBySeriesId() {
+    var library = libraryRepository.save(LibraryFixtureCreator.buildFakeLibrary());
+    var director =
+        personRepository.save(
+            Person.builder().name("Series Director").sourceId("series-director-1").build());
+    var series =
+        seriesRepository.saveAndFlush(
+            Series.builder()
+                .title("Director Series")
+                .library(library)
+                .directors(List.of(director))
+                .build());
+
+    var directors = personRepository.findDirectorsBySeriesId(series.getId());
+
+    assertThat(directors).singleElement().extracting(Person::getName).isEqualTo("Series Director");
   }
 }
