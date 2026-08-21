7a23ad4533683de081eabc91821f04c0b634b16f
Andrew Stucky <andrew.david.stucky@gmail.com>
2026-08-21 12:57:23 -0400

    behavioral: align sharing payloads after rebase

--- src/main/java/com/streamarr/server/graphql/mutation/sharing/AcceptProfileSharePayload.java
+++ src/main/java/com/streamarr/server/graphql/mutation/sharing/AcceptProfileSharePayload.java
@@ -2,6 +2,7 @@ package com.streamarr.server.graphql.mutation.sharing;
 
 import com.streamarr.server.graphql.dto.ProfileShareView;
 import java.util.List;
+import java.util.Optional;
 
 public record AcceptProfileSharePayload(
-    ProfileShareView share, List<AcceptProfileShareError> userErrors) {}
+    Optional<ProfileShareView> share, List<AcceptProfileShareError> userErrors) {}
--- src/main/java/com/streamarr/server/graphql/mutation/sharing/RejectProfileSharePayload.java
+++ src/main/java/com/streamarr/server/graphql/mutation/sharing/RejectProfileSharePayload.java
@@ -2,6 +2,7 @@ package com.streamarr.server.graphql.mutation.sharing;
 
 import com.streamarr.server.graphql.dto.ProfileShareView;
 import java.util.List;
+import java.util.Optional;
 
 public record RejectProfileSharePayload(
-    ProfileShareView share, List<RejectProfileShareError> userErrors) {}
+    Optional<ProfileShareView> share, List<RejectProfileShareError> userErrors) {}
