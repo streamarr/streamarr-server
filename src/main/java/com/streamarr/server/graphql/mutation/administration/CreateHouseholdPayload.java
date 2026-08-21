f133f5d1e197c55381523f2da9c4e8d262dd9494
Andrew Stucky <andrew.david.stucky@gmail.com>
2026-08-21 12:50:19 -0400

    behavioral: align administration payloads after rebase

--- src/main/java/com/streamarr/server/graphql/mutation/administration/CreateHouseholdPayload.java
+++ src/main/java/com/streamarr/server/graphql/mutation/administration/CreateHouseholdPayload.java
@@ -2,6 +2,7 @@ package com.streamarr.server.graphql.mutation.administration;
 
 import com.streamarr.server.graphql.dto.HouseholdAdministration;
 import java.util.List;
+import java.util.Optional;
 
 public record CreateHouseholdPayload(
-    HouseholdAdministration household, List<CreateHouseholdError> userErrors) {}
+    Optional<HouseholdAdministration> household, List<CreateHouseholdError> userErrors) {}
--- src/main/java/com/streamarr/server/graphql/mutation/administration/GrantServerAdminPayload.java
+++ src/main/java/com/streamarr/server/graphql/mutation/administration/GrantServerAdminPayload.java
@@ -2,6 +2,7 @@ package com.streamarr.server.graphql.mutation.administration;
 
 import com.streamarr.server.graphql.dto.AccountAdministration;
 import java.util.List;
+import java.util.Optional;
 
 public record GrantServerAdminPayload(
-    AccountAdministration account, List<GrantServerAdminError> userErrors) {}
+    Optional<AccountAdministration> account, List<GrantServerAdminError> userErrors) {}
