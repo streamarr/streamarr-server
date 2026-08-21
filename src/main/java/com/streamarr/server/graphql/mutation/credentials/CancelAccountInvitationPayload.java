baca723c2d39fa3b1c6070d97f6921bd3684c717
Andrew Stucky <andrew.david.stucky@gmail.com>
2026-08-21 12:55:14 -0400

    behavioral: align credential payloads after rebase

--- src/main/java/com/streamarr/server/graphql/mutation/credentials/CancelAccountInvitationPayload.java
+++ src/main/java/com/streamarr/server/graphql/mutation/credentials/CancelAccountInvitationPayload.java
@@ -2,6 +2,7 @@ package com.streamarr.server.graphql.mutation.credentials;
 
 import com.streamarr.server.graphql.dto.AccountInvitationView;
 import java.util.List;
+import java.util.Optional;
 
 public record CancelAccountInvitationPayload(
-    AccountInvitationView invitation, List<CancelAccountInvitationError> userErrors) {}
+    Optional<AccountInvitationView> invitation, List<CancelAccountInvitationError> userErrors) {}
--- src/main/java/com/streamarr/server/graphql/mutation/credentials/IssuePasswordResetPayload.java
+++ src/main/java/com/streamarr/server/graphql/mutation/credentials/IssuePasswordResetPayload.java
@@ -2,6 +2,7 @@ package com.streamarr.server.graphql.mutation.credentials;
 
 import com.streamarr.server.graphql.dto.IssuedPasswordReset;
 import java.util.List;
+import java.util.Optional;
 
 public record IssuePasswordResetPayload(
-    IssuedPasswordReset issued, List<IssuePasswordResetError> userErrors) {}
+    Optional<IssuedPasswordReset> issued, List<IssuePasswordResetError> userErrors) {}
