ca0d637d39f8455d2255417678c9f5023704ce12
Andrew Stucky <andrew.david.stucky@gmail.com>
2026-08-21 13:06:47 -0400

    behavioral: align device logout and payloads after rebase

--- src/main/java/com/streamarr/server/graphql/mutation/devices/BlockEsnPayload.java
+++ src/main/java/com/streamarr/server/graphql/mutation/devices/BlockEsnPayload.java
@@ -2,5 +2,6 @@ package com.streamarr.server.graphql.mutation.devices;
 
 import com.streamarr.server.graphql.dto.EsnBlockView;
 import java.util.List;
+import java.util.Optional;
 
-public record BlockEsnPayload(EsnBlockView block, List<BlockEsnError> userErrors) {}
+public record BlockEsnPayload(Optional<EsnBlockView> block, List<BlockEsnError> userErrors) {}
