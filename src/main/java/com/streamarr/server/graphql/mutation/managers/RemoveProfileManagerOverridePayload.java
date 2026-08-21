70845af117eb378a1d48a2c182627041264620a0
Andrew Stucky <andrew.david.stucky@gmail.com>
2026-08-21 12:59:23 -0400

    behavioral: align manager payloads after rebase

--- src/main/java/com/streamarr/server/graphql/mutation/managers/RemoveProfileManagerOverridePayload.java
+++ src/main/java/com/streamarr/server/graphql/mutation/managers/RemoveProfileManagerOverridePayload.java
@@ -1,6 +1,7 @@
 package com.streamarr.server.graphql.mutation.managers;
 
 import java.util.List;
+import java.util.Optional;
 
 public record RemoveProfileManagerOverridePayload(
-    String profileId, List<RemoveProfileManagerOverrideError> userErrors) {}
+    Optional<String> profileId, List<RemoveProfileManagerOverrideError> userErrors) {}
