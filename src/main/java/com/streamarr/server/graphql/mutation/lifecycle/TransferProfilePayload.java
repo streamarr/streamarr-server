00112c762db965b1ad7857d8d253f7807cb3e5a5
Andrew Stucky <andrew.david.stucky@gmail.com>
2026-08-21 13:08:55 -0400

    behavioral: align lifecycle payloads after rebase

--- src/main/java/com/streamarr/server/graphql/mutation/lifecycle/TransferProfilePayload.java
+++ src/main/java/com/streamarr/server/graphql/mutation/lifecycle/TransferProfilePayload.java
@@ -2,6 +2,7 @@ package com.streamarr.server.graphql.mutation.lifecycle;
 
 import com.streamarr.server.graphql.dto.ProfileAdministration;
 import java.util.List;
+import java.util.Optional;
 
 public record TransferProfilePayload(
-    ProfileAdministration profile, List<TransferProfileError> userErrors) {}
+    Optional<ProfileAdministration> profile, List<TransferProfileError> userErrors) {}
